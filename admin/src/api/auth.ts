38a646b1af3cad3063f399d79c43289ccb15249b
gaoweibao <gaoweibao@eversec.cn>
2020-09-03 16:57:58 +0800

    配置next开发跨域代理转发

--- admin/src/api/auth.ts
+++ admin/src/api/auth.ts
@@ -1,8 +1,10 @@
+import qs from "qs"
+
 export default {
     async login(o:any){
         const res = await fetch('/api/login',{
             method:"POST",
-            body:o
+            body:qs.stringify(o)
         })
         return  res.json()
     }
--- admin/src/server.js
+++ admin/src/server.js
@@ -1,12 +1,12 @@
 // server.js
-const { createServer } = require('http')
+const express = require('express')
 const { parse } = require('url')
 const next = require('next')
-
 const { createProxyMiddleware } = require('http-proxy-middleware')
+
 const devProxy = {
     '/api': {
-        target: 'http://localhost:5501', // 端口自己配置合适的
+        target: 'http://localhost:5501/', // 端口自己配置合适的
         pathRewrite: {
             '^/api': '/'
         },
@@ -18,16 +18,28 @@ const dev = process.env.NODE_ENV !== 'production'
 const app = next({ dev })
 const handle = app.getRequestHandler()
 
-app.prepare().then(() => {
-  createServer((req, res) => {
+app.prepare()
+.then(() => {
+  
+  const server = express();
+
+  // 代理
+  Object.keys(devProxy).forEach(function(context) {
+    server.use(context,createProxyMiddleware(devProxy[context]))
+  });
+    
+
+  server.use((req, res) => {
     const parsedUrl = parse(req.url, true)
-    // 代理
-    Object.keys(devProxy).forEach(function(context) {
-        createProxyMiddleware(context, devProxy[context])
-    })
-    handle(req, res, parsedUrl)
-  }).listen(3000, (err) => {
+     return handle(req, res, parsedUrl)
+  });
+
+  server.listen(3000, (err) => {
     if (err) throw err
     console.log('> Ready on http://localhost:3000')
   })
+
+})
+.catch((err)=>{
+  console.log(err)
 })
