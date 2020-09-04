650819804471c392ad5ff001990a39207bc2b826
gaoweibao <gaoweibao@eversec.cn>
2020-09-04 18:40:14 +0800

    注册页面增加

--- admin/pages/register/index.tsx
+++ admin/pages/register/index.tsx
@@ -1,9 +1,17 @@
 import Link from 'next/link'
 import React, { useState } from 'react';
-import { Button ,Input  } from 'antd';
+import { Form, Input, Button, Checkbox } from 'antd';
 import { UserOutlined ,KeyOutlined } from '@ant-design/icons';
 import styles from "./Login.module.scss";
 import http_auth from "../../src/api/auth";
+
+const layout = {
+  labelCol: { span: 8 },
+  wrapperCol: { span: 16 },
+};
+const tailLayout = {
+  wrapperCol: { offset: 8, span: 16 },
+};
 
 export default function Home() {
   // 增加用户名 密码
@@ -34,32 +42,46 @@ export default function Home() {
   function vertifyPassword(event:any) {
     setVertifyPwd(event.target.value);
   }
+  const onFinish = (values:string) => {
+    console.log('Success:', values);
+  };
+
+  const onFinishFailed = (errorInfo:any) => {
+    console.log('Failed:', errorInfo);
+  };
   return (
       <main className={styles["main-container"]}>
         <div className={styles["login-container"]}>
           <h2 className={styles["login-tip"]}>用户注册</h2>
-          <Input 
-            size="large" 
-            placeholder="用户名" 
-            className={styles["item"]} 
-            value={name} 
-            onChange={handleChange} 
-            prefix={<UserOutlined />} />
-          <Input.Password 
-            size="large" 
-            placeholder="密码" 
-            className={styles["item"]} 
-            value={password} 
-            onChange={handleChangepassword} 
-            prefix={<KeyOutlined />}/>
-          <Input.Password 
-            size="large" 
-            placeholder="确认密码" 
-            className={styles["item"]} 
-            value={vertifyPwd} 
-            onChange={vertifyPassword} 
-            prefix={<KeyOutlined />} />
-          <Button type="primary" size="large" className={styles["item"]} block onClick={register}>登录</Button>
+          <Form
+            {...layout}
+            name="basic"
+            initialValues={{ remember: true }}
+            onFinish={onFinish}
+            onFinishFailed={onFinishFailed}
+          >
+            <Form.Item
+              label="Username"
+              name="username"
+              rules={[{ required: true, message: 'Please input your username!' }]}
+            >
+              <Input size="large" />
+            </Form.Item>
+
+            <Form.Item
+              label="Password"
+              name="password"
+              rules={[{ required: true, message: 'Please input your password!' }]}
+            >
+              <Input.Password size="large" />
+            </Form.Item>
+
+            <Form.Item {...tailLayout}>
+              <Button type="primary" size="large" htmlType="submit">
+                Submit
+              </Button>
+            </Form.Item>
+          </Form>
           <Link href="/login">
             <a>去登录</a>
           </Link>
